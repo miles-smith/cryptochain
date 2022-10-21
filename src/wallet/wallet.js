17d0791ce50c52475a2ca7e8681abb29cbfb1748
Miles Smith <miles-smith@users.noreply.github.com>
2022-10-21 15:31:24 +0100

    Allow wallets to create transactions

--- src/wallet/wallet.js
+++ src/wallet/wallet.js
@@ -1,4 +1,5 @@
-const { INITIAL_BALANCE } = require('../../config');
+const Transaction                   = require('./transaction');
+const { INITIAL_BALANCE }           = require('../../config');
 const { ellipticCurve, cryptoHash } = require('../utils');
 
 class Wallet {
@@ -12,6 +13,13 @@ class Wallet {
     const signature = this.keyPair.sign(cryptoHash(data));
 
     return signature.toDER('hex');
+  }
+
+  createTransaction({ recipient, amount }) {
+    if(this.balance < amount)
+      throw new Error('Insufficient funds!');
+
+    return new Transaction({ sender: this, recipient, amount });
   }
 }
 
--- src/wallet/wallet.test.js
+++ src/wallet/wallet.test.js
@@ -1,4 +1,5 @@
-const Wallet = require('./wallet');
+const Wallet              = require('./wallet');
+const Transaction         = require('./transaction');
 const { INITIAL_BALANCE } = require('../../config');
 const { verifySignature } = require('../utils');
 
@@ -33,6 +34,41 @@ describe('Wallet', () => {
       const verification = verifySignature({ publicKey: wallet.publicKey, data, signature });
 
       expect(verification).toBe(false);
+    });
+  });
+
+  describe('createTransaction()', () => {
+    let recipient = 'test-recipient';
+
+    describe('and the amount exceeds the balance', () => {
+      it('throws an error', () => {
+        const amount = wallet.balance + 1;
+
+        expect(() => wallet.createTransaction({ recipient , amount }))
+          .toThrow('Insufficient funds!');
+      });
+    })
+
+    describe('and there is sufficient balance', () => {
+      let transaction;
+      let amount;
+
+      beforeEach(() => {
+        amount      = wallet.balance - 1;
+        transaction = wallet.createTransaction({ recipient, amount });
+      });
+
+      it('creates a `Transaction`', () => {
+        expect(transaction).toBeInstanceOf(Transaction);
+      });
+
+      it('matches the transaction input with the wallet', () => {
+        expect(transaction.input.address).toEqual(wallet.publicKey);
+      });
+
+      it('oututs the amount to the recipient', () => {
+        expect(transaction.output[recipient]).toEqual(amount);
+      });
     });
   });
 });
